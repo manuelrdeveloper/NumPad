37f38ea56d008d6dcdb4934c1e521a20a3385978
manuel antonio calderon ramirez <manuel.r.developer@gmail.com>
2026-05-09 20:16:25 -0600

    Agrega NumPad

--- src/main.py
+++ src/main.py
@@ -2,24 +2,9 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
-
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            expand=True,
-            content=ft.Container(
-                content=counter,
-                alignment=ft.Alignment.CENTER,
-            ),
-        )
-    )
+    page.title = "NumPad"
+    img = ft.Image(src="assets/logo.png")
+    page.add(img)
 
 
-ft.run(main)
+ft.app(target=main)
